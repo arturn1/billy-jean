f27b20c1fb020f60edf7eee5b1aac83a3f4779ff
Artur <31557902+arturn1@users.noreply.github.com>
2024-08-14 01:04:15 -0300

    update

--- src/services/QrCodeService.ts
+++ src/services/QrCodeService.ts
@@ -32,7 +32,16 @@ export const QrCodeService = {
       }
   
       const data = await response.json();
-      const respQrCode = createQrCode(data?.success, false, "", data?.message) as QrCodeModel;
+      if (containsStringInObject(data, ["false", "null", "session_not_found"])) {
+        const session = await QrCodeService.setSession(QrCodeData);
+        const respQrCode = await createQrCode(data?.success, false, "", data?.message) as QrCodeModel;
+      }
+      else if (containsStringInObject(data, ["false", "null", "session_not_connected"]))
+      {
+
+      }else{
+        
+      }
   
       return respQrCode;
     } catch (error) {
@@ -45,11 +54,47 @@ export const QrCodeService = {
     // Aqui você pode implementar a lógica para obter o QR Code
   },
   
-  setSession: (sessionId: string, apikey: string) => {
-    // Aqui você pode implementar a lógica para definir a sessão
+  setSession: async (QrCodeData: QrCodeModel): Promise<QrCodeModel> => {
+    const route = `${endpoint}${sessionRoutes.start}/${QrCodeData.sessionId}`;
+    const headers = {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Headers": "Authorization", 
+      "Access-Control-Allow-Methods": "GET, POST, OPTIONS, PUT, PATCH, DELETE",
+      "Content-Type": "application/json;charset=UTF-8",
+      'Accept': 'application/json',
+      'x-api-key': QrCodeData.apiKey, 
+    };
+  
+    try {
+      const response = await fetch(route, {
+        method: 'GET',
+        headers: headers,
+      });
+  
+      if (!response.ok) {
+        throw new Error('Não há conexão com a internet.');
+      }
+  
+      const data = await response.json();
+      const respQrCode = await createQrCode(data?.success, false, "", data?.message) as QrCodeModel;
+      
+      return respQrCode;
+    } catch (error) {
+      console.error('Falha ao recuperar a sessão:', error);
+      throw error;
+    }
   },
+  
 
   terminateAllSession: (sessionId: string, apikey: string) => {
     // Aqui você pode implementar a lógica para terminar todas as sessões
   },
+
 };
+
+const containsStringInObject = (object: { [key: string]: any }, stringsToCheck: string[]): boolean => {
+  return Object.values(object).some(value => 
+    stringsToCheck.includes(value)
+  );
+}
+
